306dc29c44078d15d5eb3042fc8d79cafba385cb
Fred Westling <fwes7558@uni.sydney.edu.au>
2019-04-17 09:32:45 +1000

    Updates

---
+++ precache-manifest.7cd0bb7cc38d2fcf0118cfc05cf30903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77d81e9fb980c79aec4f",
+    "url": "/award-soundboard/static/css/main.135cb858.chunk.css"
+  },
+  {
+    "revision": "77d81e9fb980c79aec4f",
+    "url": "/award-soundboard/static/js/main.bf2ccebd.chunk.js"
+  },
+  {
+    "revision": "7cb4d3548494a6f769cc",
+    "url": "/award-soundboard/static/js/runtime~main.bf8de04b.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/award-soundboard/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/award-soundboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2fa99abaa87444635cdd7ddc353f0994",
+    "url": "/award-soundboard/index.html"
+  }
+];
