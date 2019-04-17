d0ae161517881fcd9c2fc5f097a8ad9fda9a574d
Fred Westling <fwes7558@uni.sydney.edu.au>
2019-04-17 15:27:20 +1000

    Updates

---
+++ precache-manifest.fc19770059428e4ddf2565e651fe1b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c8fc36e76f7fe2585ef",
+    "url": "/award-soundboard/static/css/main.d7d90fd8.chunk.css"
+  },
+  {
+    "revision": "5c8fc36e76f7fe2585ef",
+    "url": "/award-soundboard/static/js/main.378dd0ae.chunk.js"
+  },
+  {
+    "revision": "7cb4d3548494a6f769cc",
+    "url": "/award-soundboard/static/js/runtime~main.bf8de04b.js"
+  },
+  {
+    "revision": "b3e79233df8e46d9345e",
+    "url": "/award-soundboard/static/css/2.f35cd30e.chunk.css"
+  },
+  {
+    "revision": "b3e79233df8e46d9345e",
+    "url": "/award-soundboard/static/js/2.f2e779b6.chunk.js"
+  },
+  {
+    "revision": "c9e55a395c3d57e23f9ab36e62772c30",
+    "url": "/award-soundboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/award-soundboard/precache-manifest.77e510b03f9a168a8cd7532e93446837.js"
+  "/award-soundboard/precache-manifest.fc19770059428e4ddf2565e651fe1b48.js"
 );
 
 workbox.clientsClaim();
