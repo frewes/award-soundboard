ee0a2819501225d0f734d29deb8e72c49ef0636b
Fred Westling <fwes7558@uni.sydney.edu.au>
2019-05-06 15:50:58 +1000

    Actually let it sound

--- src/Track.js
+++ src/Track.js
@@ -6,15 +6,23 @@ export default class Track extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            runtime: this.props.track.runtime,
             selected: false,
             playing: false,
             volume: 50
         };
         this.click = this.click.bind(this);
+        this.playSound = this.playSound.bind(this);
+        this.stopSound = this.stopSound.bind(this);
+        this.updateRuntime = this.updateRuntime.bind(this);
     }
 
     click(evt) {
         console.log(this.props.track);
+        if (this.state.selected) 
+          this.stopSound();
+        else
+          this.playSound();
         this.setState({selected: !this.state.selected});
     }
 
@@ -26,17 +34,22 @@ export default class Track extends Component {
       this.setState({playing: false});
     }
 
+    updateRuntime(evt) {
+      this.setState({runtime: Math.round((evt.duration - evt.position)/1000)})
+    }
+
   render() {
     return (
       <tr className={this.state.selected ? "clicked" : ""} onClick={this.click}>
           <td>{this.props.track.title}</td>
           <td>{this.props.track.artist}</td>
-          <td>{this.props.track.runtime} s</td>
+          <td>{this.state.runtime} s</td>
           <Sound
             url={this.props.track.path}
             playStatus={this.state.playing ? Sound.status.PLAYING : Sound.status.STOPPED}
             volume={this.state.volume}
             autoLoad={true}
+            onPlaying={this.updateRuntime}
           />
       </tr>
 	);
